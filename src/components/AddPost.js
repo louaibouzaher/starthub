732f33152b1696e654e4a670e165f21f5582794c
louaibouzaher <jm_bouzaher@esi.dz>
2022-02-10 15:00:28 +0200

    Made all post state in one object

--- src/components/AddPost.js
+++ src/components/AddPost.js
@@ -1,21 +1,37 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const AddPost = () => {
+  const [post, setPost] = useState({
+    title: '',
+    description: '',
+    image: null,
+  })
+  const handleChange = (e) => {
+    setPost({
+      ...post,
+      [e.target.name]: e.target.value,
+    })
+  }
+
   const handleFile = (e) => {
-    console.log(e)
-    setFile(e.target.files[0])
+    setPost({ ...post, image: e.target.files[0] })
     // TODO: Upload file and send it to backend
     // const fr = new FileReader()
     // fr.onload = () => {
     // }
   }
-  const [file, setFile] = useState(null)
+
   const labelUpload = 'Seems empty here 🤔'
   return (
     <div className="flex flex-col">
       <div className="flex flex-col w-1/2">
         <label>Title</label>
-        <input type="text" className="border-2 border-dark p-2 rounded-md" />
+        <input
+          type="text"
+          className="border-2 border-dark p-2 rounded-md"
+          name="title"
+          onChange={handleChange}
+        />
       </div>
       <div className="flex flex-col w-2/3 mt-4">
         <label>Description</label>
@@ -24,6 +40,8 @@ export const AddPost = () => {
             resize: 'none',
           }}
           className="h-36 border-2 border-dark p-4 rounded-md"
+          name="description"
+          onChange={handleChange}
         />
       </div>
 
@@ -41,7 +59,7 @@ export const AddPost = () => {
         >
           Upload an image
         </label>
-        <div className="mx-2 text-xs"> {file?.name || labelUpload}</div>
+        <div className="mx-2 text-xs"> {post.image?.name || labelUpload}</div>
       </div>
     </div>
   )
