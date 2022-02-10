9cea1c833a3b2fd06382244c0a22921faf35ecd8
louaibouzaher <jm_bouzaher@esi.dz>
2022-02-10 14:47:41 +0200

    Made all demo state in one object

--- src/components/AddDemo.js
+++ src/components/AddDemo.js
@@ -4,44 +4,41 @@ import ButtonArrow from '../assets/icons/ButtonArrow'
 import DesktopDatePicker from '@mui/lab/DesktopDatePicker'
 import AdapterDateFns from '@mui/lab/AdapterDateFns'
 import LocalizationProvider from '@mui/lab/LocalizationProvider'
-import Slider from '@mui/material/Slider'
 import Autocomplete from '@mui/material/Autocomplete'
 import TextField from '@mui/material/TextField'
 import { countries } from '../data/countries'
-import { createTheme, ThemeProvider } from '@mui/material/styles'
-import tailwindConfig from '../../tailwind.config'
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: tailwindConfig.theme.extend.colors.purple,
-    },
-  },
-})
-
-function valuetext(value) {
-  return `${value}$`
-}
 
 export const AddDemo = () => {
   const [step, setStep] = useState(1)
+
+  const [demo, setDemo] = useState({
+    title: '',
+    description: '',
+    tags: '',
+    video: null,
+    showYoutube: false,
+    youtubeLink: null,
+    isEstablished: true,
+    establishedOn: new Date(),
+    estimatedCapital: 0,
+    location: null,
+  })
   const [file, setFile] = useState(null)
   const [showYoutube, setShowYoutube] = useState(false)
-  const [value, setValue] = React.useState(new Date('2014-08-18T21:11:54'))
-  const [valueSlider, setValueSlider] = useState([200000, 5000000])
   const [isDatePickerDisabled, setIsDatePickerDisabled] = useState(false)
 
-  const handleChange = (newValue) => {
-    setValue(newValue)
-  }
-
-  const handleChangeSlider = (event, newValue) => {
-    setValueSlider(newValue)
+  const handleChange = (e) => {
+    console.log(demo)
+    const value = e.target.value
+    setDemo({
+      ...demo,
+      [e.target.name]: value,
+    })
   }
 
   const handleFile = (e) => {
     console.log(e)
-    setFile(e.target.files[0])
+    setDemo({ ...demo, video: e.target.files[0] })
     // TODO: Upload file and send it to backend
     // const fr = new FileReader()
     // fr.onload = () => {
@@ -55,7 +52,12 @@ export const AddDemo = () => {
         <div className="flex flex-col">
           <div className="flex flex-col w-1/2">
             <label>Title</label>
-            <input type="text" className="border-2 border-dark p-2 rounded-md" />
+            <input
+              type="text"
+              className="border-2 border-dark p-2 rounded-md"
+              name="title"
+              onChange={handleChange}
+            />
           </div>
           <div className="flex flex-col w-2/3 mt-4">
             <label>Description</label>
@@ -64,21 +66,28 @@ export const AddDemo = () => {
                 resize: 'none',
               }}
               className="h-36 border-2 border-dark p-4 rounded-md"
+              name="description"
+              onChange={handleChange}
             />
           </div>
           <div className="flex flex-col w-1/2 mt-4">
             <label>Tags</label>
             <p className="font-light text-xs">Provide values separated by commas.</p>
-            <input type="text" className="border-2 border-dark p-2 rounded-md" />
+            <input
+              type="text"
+              className="border-2 border-dark p-2 rounded-md"
+              name="tags"
+              onChange={handleChange}
+            />
           </div>
 
           <div className="flex flex-row items-center mt-2">
             <input
               accept="video/*"
               type="file"
-              id="file"
               className="hidden"
-              onChange={(e) => handleFile(e)}
+              name="file"
+              onChange={handleFile}
             />
             <label
               for="file"
@@ -92,8 +101,9 @@ export const AddDemo = () => {
           <div>
             <Button
               label="YouTube Link"
-              onClick={() => setShowYoutube(!showYoutube)}
               btnStyle=" text-gray-500 mt-4 "
+              name="showYoutube"
+              onClick={() => setShowYoutube(!showYoutube)}
               Icon={() => (
                 <ButtonArrow color="gray" className={showYoutube ? ' rotate-180 ' : ''} />
               )}
@@ -102,7 +112,12 @@ export const AddDemo = () => {
           </div>
           {showYoutube && (
             <div className="flex flex-col w-1/2">
-              <input type="text" className="border-2 border-gray-500 p-2 rounded-md" />
+              <input
+                type="text"
+                className="border-2 border-gray-500 p-2 rounded-md"
+                name="youtubeLink"
+                onChange={handleChange}
+              />
             </div>
           )}
         </div>
@@ -116,8 +131,8 @@ export const AddDemo = () => {
                 disabled={isDatePickerDisabled}
                 label="Select Date"
                 inputFormat="MM/dd/yyyy"
-                value={value}
-                onChange={handleChange}
+                value={demo.establishedOn}
+                onChange={(newValue) => setDemo({ ...demo, establishedOn: newValue })}
                 renderInput={(params) => <TextField {...params} />}
               />
             </LocalizationProvider>
@@ -128,35 +143,33 @@ export const AddDemo = () => {
                 (isDatePickerDisabled ? 'bg-purple text-white shadow-lg' : 'text-purple')
               }
               onClick={() => {
+                setDemo({
+                  ...demo,
+                  isEstablished: !demo.isEstablished,
+                  establishedOn: null,
+                })
                 setIsDatePickerDisabled(!isDatePickerDisabled)
               }}
             />
           </div>
           <div className="flex w-full items-center mt-4">
             <div className="mr-4 min-w-max">Estimated Capital</div>
-            <div className="w-full mx-4 mt-2">
-              <ThemeProvider theme={theme}>
-                <Slider
-                  min={0}
-                  max={10000000}
-                  step={1000000}
-                  getAriaLabel={() => 'Capital Range'}
-                  value={valueSlider}
-                  onChange={handleChangeSlider}
-                  valueLabelDisplay="auto"
-                  getAriaValueText={valuetext}
-                  color="primary"
-                />
-              </ThemeProvider>
-            </div>
+            <input
+              type="number"
+              className="border-2 border-dark p-2 rounded-md mx-1 text-purple text-center"
+              name="estimatedCapital"
+              onChange={handleChange}
+            />
+            <span>$</span>
           </div>
           <div className="flex flex-col w-1/2 mt-4">
-            <label className="my-2"> Location </label>
+            <label> Location </label>
             <Autocomplete
               disablePortal
               id="combo-box-demo"
               options={countries}
               sx={{ width: 300 }}
+              onChange={(e) => setDemo({ ...demo, location: e.target.innerHTML })}
               renderInput={(params) => <TextField {...params} label="Select a country" />}
             />{' '}
           </div>
