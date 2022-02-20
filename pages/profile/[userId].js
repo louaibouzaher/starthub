23302e8cccda66423cd7e98caf4aafdc85c3b761
louaibouzaher <jm_bouzaher@esi.dz>
2022-02-20 14:36:35 +0200

    Added link to profile

--- pages/profile/[userId].js
+++ pages/profile/[userId].js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import { useRouter } from 'next/router'
+import Head from 'next/head'
 
+import { Navbar } from '../../src/components/Navbar'
 import ProjectsIcon from '../../src/assets/icons/ProjectsIcon'
 import PostsIcon from '../../src/assets/icons/PostsIcon'
 import { SectionIndexer } from '../../src/components/SectionIndexer'
@@ -29,76 +31,86 @@ export default function Profile() {
   ]
 
   return (
-    <div className="bg-slate-50 w-full p-10 px-60 min-h-screen text-dark">
-      <div className="flex justify-center items-start bg-gray-100 rounded-md shadow-md py-6 px-4 ">
-        <UserAvatar sizing link={connectedUser.picture} className="h-32 w-32 my-2" />
-        <div className="flex flex-col justify-start items-start mx-10 p-2 w-2/5">
-          <div className="text-2xl p-2 font-bold">
-            {connectedUser.firstName} {connectedUser.lastName}
+    <>
+      <Head>
+        <title>
+          {connectedUser.firstName} {connectedUser.lastName} - StartHub
+        </title>
+      </Head>
+      <Navbar isConnected connectedUser={connectedUser} />
+      <div className=" w-full pt-20 px-60 min-h-screen text-dark">
+        <div className="flex justify-center items-start  rounded-md shadow-md py-6 px-4 ">
+          <UserAvatar sizing link={connectedUser.picture} className="h-32 w-32 my-2" />
+          <div className="flex flex-col justify-start items-start mx-10 p-2 w-2/5">
+            <div className="text-2xl p-2 font-bold">
+              {connectedUser.firstName} {connectedUser.lastName}
+            </div>
+            <div className="text-xs text-gray-400 px-2">{connectedUser.position}</div>
+            <div className="text-sm p-2 break-words">{connectedUser.biography}</div>
+            <div className="flex w-full mt-2">
+              <Button
+                btnStyle="w-1/2 bg-purple border-2 border-purple text-white hover:text-purple hover:bg-white mx-1"
+                label="Follow"
+                onClick={() => {
+                  console.log('Follow')
+                }}
+              />
+              <Button
+                btnStyle="w-1/2 bg-white border-2 border-purple text-purple mx-1"
+                label="Message"
+                onClick={() => {
+                  console.log('Follow')
+                }}
+              />
+            </div>
           </div>
-          <div className="text-xs text-gray-400 px-2">{connectedUser.position}</div>
-          <div className="text-sm p-2 break-words">{connectedUser.biography}</div>
-          <div className="flex w-full mt-2">
-            <Button
-              btnStyle="w-1/2 bg-purple border-2 border-purple text-white hover:text-purple hover:bg-white mx-1"
-              label="Follow"
-              onClick={() => {
-                console.log('Follow')
-              }}
-            />
-            <Button
-              btnStyle="w-1/2 bg-white border-2 border-purple text-purple mx-1"
-              label="Message"
-              onClick={() => {
-                console.log('Follow')
-              }}
-            />
+          <div className="flex justify-center items-center mx-10 text-xl">
+            <div className="flex flex-col  justify-center item-center ">
+              <div className="flex flex-col items-center p-2">
+                <div>Followers</div>
+                <div className="text-purple">32</div>
+              </div>
+              <div className="flex flex-col items-center p-2">
+                <div>Following</div>
+                <div className="text-purple">763</div>
+              </div>
+            </div>
+            <div className="flex flex-col justify-center item-center p-2">
+              <div className="flex flex-col items-center p-2">
+                <div>Posts</div>
+                <div className="text-purple">4</div>
+              </div>
+              <div className="flex flex-col items-center p-2">
+                <div>Projects</div>
+                <div className="text-purple">1</div>
+              </div>
+            </div>
           </div>
         </div>
-        <div className="flex justify-center items-center mx-10 text-xl">
-          <div className="flex flex-col  justify-center item-center ">
-            <div className="flex flex-col items-center p-2">
-              <div>Followers</div>
-              <div className="text-purple">32</div>
-            </div>
-            <div className="flex flex-col items-center p-2">
-              <div>Following</div>
-              <div className="text-purple">763</div>
-            </div>
-          </div>
-          <div className="flex flex-col justify-center item-center p-2">
-            <div className="flex flex-col items-center p-2">
-              <div>Posts</div>
-              <div className="text-purple">4</div>
-            </div>
-            <div className="flex flex-col items-center p-2">
-              <div>Projects</div>
-              <div className="text-purple">1</div>
-            </div>
-          </div>
+        <div className="w-full my-4 min-h-screen rounded-md">
+          <SectionIndexer
+            section={section}
+            changeSection={setSection}
+            sections={sections}
+          />
+          {section === 0 &&
+            connectedUser.posts.map((post) => (
+              <Post
+                user={post.user}
+                time={post.time}
+                picture={post.picture}
+                content={post.content}
+                title={post.title}
+                isOwnPost
+              />
+            ))}
+          {section === 1 &&
+            connectedUser.projects.map((p) => (
+              <Project project={p} user={p.user} isOwnProject />
+            ))}
         </div>
       </div>
-      <div className="w-full my-4 min-h-screen rounded-md">
-        <SectionIndexer
-          section={section}
-          changeSection={setSection}
-          sections={sections}
-        />
-        {section === 0 &&
-          connectedUser.posts.map((post) => (
-            <Post
-              user={post.user}
-              time={post.time}
-              picture={post.picture}
-              content={post.content}
-              isOwnPost
-            />
-          ))}
-        {section === 1 &&
-          connectedUser.projects.map((p) => (
-            <Project project={p} user={p.user} isOwnProject />
-          ))}
-      </div>
-    </div>
+      )
+    </>
   )
 }
--- src/data/user.js
+++ src/data/user.js
@@ -16,11 +16,11 @@ export const connectedUser = {
       title: 'Checkout the latest news about our UNICORN 🦄',
       content: `#MENA #Investment #TechStartup`,
       picture:
-        'https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+        'https://images.unsplash.com/photo-1559136555-9303baea8ebd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
       time: Math.floor(Math.random() * 15) + 1 + ' hours ago',
     },
     {
-      title: 'This is the post Title',
+      title: 'Breaking News!',
       user: {
         avatar:
           'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
