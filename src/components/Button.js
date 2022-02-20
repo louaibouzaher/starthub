9e5a12db1a7f3fad3efa0eb93c26c8623cec34a2
louaibouzaher <jm_bouzaher@esi.dz>
2022-02-21 01:00:25 +0200

    Make buttons smaller across the app

--- src/components/Button.js
+++ src/components/Button.js
@@ -7,7 +7,7 @@ export const Button = React.forwardRef(
         href={href}
         onClick={() => onClick()}
         className={
-          ' flex justify-center items-center cursor-pointer font-sans py-2 px-8 rounded-md ' +
+          ' flex justify-center items-center cursor-pointer font-sans py-2 px-6 rounded-md text-sm ' +
           btnStyle
         }
       >
