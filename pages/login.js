3095eab09ed53239b35c4dc83405da80719e6cd9
Bilal H. Wehbi <bhw03@mail.aub.edu>
2022-02-24 22:36:02 +0200

    login edits

--- pages/login.js
+++ pages/login.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import WhiteLogo from '../src/assets/images/WhiteLogo'
 import { Button } from '../src/components/Button'
 import Link from 'next/link'
 
@@ -8,12 +9,16 @@ export default function Login() {
       <Head>
         <title>Login - StartHub</title>
       </Head>
-      <div class="h-screen bg-gradient-to-br from-blue-600 to-indigo-600 flex justify-center items-center w-full">
+      <div class="h-screen w-full bg-gradient-to-br from-blue-600 to-indigo-600 flex flex-col justify-center items-center ">
+        <div className=" w-4.5 h-24 stroke-cyan-500 scale-150">
+          <WhiteLogo className={' justify-center items-center  '} />
+        </div>
+
         <form>
           <div class="bg-white px-10 py-8 rounded-xl w-screen shadow-md max-w-sm">
             <div class="space-y-4">
               <h1 class="text-center text-2xl font-semibold text-black-600">
-                StartHub Login
+                Login to your account
               </h1>
               <div>
                 <label for="email" class="block mb-1 text-gray-600 font-semibold">
@@ -35,18 +40,19 @@ export default function Login() {
               </div>
             </div>
 
-            <div>
+            <div className="tx-2xl w-full bg-gradient-to-tr from-blue-600 to-indigo-600 rounded-md">
               <Link href="/browse" passHref>
                 <Button
                   label="Login"
-                  btnStyle={
-                    'mt-4 w-full bg-gradient-to-tr from-blue-600 to-indigo-600 text-indigo-100 py-2 rounded-md text-lg tracking-wide'
-                  }
+                  btnStyle={'mt-5  py-2 text-indigo-100  tracking-wide'}
                   onClick={() => {
                     console.log('Login')
                   }}
                 />
               </Link>
+            </div>
+            <div className='px-14 mt-10 '>
+              <p>New to StartHub? <span className='hover:text-blue-900 font-semibold active:text-blue-400'><Link href='/signup'className> SignUp</Link></span></p>
             </div>
           </div>
         </form>
