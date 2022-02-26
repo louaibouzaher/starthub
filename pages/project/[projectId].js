cd56cbca49d357a0051874a0eb6a494a1527959b
louaibouzaher <jm_bouzaher@esi.dz>
2022-02-26 03:30:09 +0200

    Fixed Project Details Page

--- pages/project/[projectId].js
+++ pages/project/[projectId].js
@@ -4,100 +4,150 @@ import { projects } from '../../src/data/projects'
 import { connectedUser } from '../../src/data/user'
 import { Navbar } from '../../src/components/Navbar'
 import { Button } from '../../src/components/Button'
-
+import Saved from '../../src/assets/icons/Saved'
+import Share from '../../src/assets/icons/Share'
+import Heart from '../../src/assets/icons/Heart'
+import Comment from '../../src/assets/icons/Comment'
 import UserAvatar from '../../src/assets/images/UserAvatar'
+import { reactionsColors, sampleComments, users } from '../../src/data/general'
+
+import { PublicationComment } from '../../src/components/PublicationDetails/PublicationComment'
 import ReactPlayer from 'react-player'
 import Location from '../../src/assets/icons/Location'
 
 const Project = ({}) => {
   const router = useRouter()
   const { projectId } = router.query
-
+  const reactions = [
+    Math.floor(Math.random() * 2),
+    Math.floor(Math.random() * 2),
+    Math.floor(Math.random() * 2),
+    Math.floor(Math.random() * 2),
+  ]
   return (
     <>
       <Navbar isConnected connectedUser={connectedUser} />
-      
-      <div className="ml-8 App flex flex-col justify-end items-end pt-16">
+      <div className="py-10 px-36 App w-full flex flex-col justify-start items-start">
+        <div className=" w-full flex justify-between mb-4 text-4xl text-dark font-bold pt-16  ">
+          <div>{projects[0].title}</div>
           <Button
-            btnStyle=" mt-8 mr-8 w-1/4 bg-purple border-2 border-purple text-white hover:text-purple hover:bg-white"
+            btnStyle=" w-1/4 bg-purple border-2 border-purple text-white hover:text-purple hover:bg-white"
             label="I am Interested"
             onClick={() => {
-            console.log('I am Interested')
+              console.log('I am Interested')
             }}
           />
-      </div>
-
-      <div className="ml-8 App w-full flex flex-col justify-start items-start">
-        <div className=" mb-4 text-4xl text-dark font-bold pt-16 hover:underline ">{projects.at(0).title}</div>
+        </div>
         <div className="flex space-x-2 justify-start ">
-        {projects.at(0).tags
-          ?.split(',')
-          .filter((t) => t.length > 0)
-          .map((t) => {
-            return (
-              <div
-                className="text-xl border-green text-green mb-4 px-2 rounded-md "
-                style={{
-                  borderWidth: 1,
-                }}
-              >
-                {' '}
-                {t}{' '}
-              </div>
-            )
-          })}
-      </div>
-      <div className="flex flex-row mt-2 items-center">
-        <Location /> <span className="my-4">{projects.at(0).location}</span>
-      </div>
-        <span className=" text-xl text-black font-bold ">Description: </span>
-        <div className={'text-green-500 no-underline my-2'}>{projects.at(0).description}
-      </div>
-      <div className='flex flex-col justify-start items-start mt-2'>
-        <div className=" text-xl text-dark my-2">
-          <div className='my-2'><span className=" text-xl text-black font-bold">Field: </span>{projects.at(0).field}</div>
-          <div className='my-2'><span className=" text-xl text-black font-bold ">Number of Employees:  </span>{projects.at(0).numberOfEmployees}</div>
-          <div className='my-2'><span className=" text-xl text-black font-bold ">Date of establishment:  </span>{projects.at(0).dateOfEstablishment}</div>
-          <div className='my-2'><span className=" text-xl text-black font-bold ">Capital:  </span>{projects.at(0).capital}
-          <span className=" text-xl text-black "> dollars  </span></div>
-          <div className='my-2'><span className=" text-xl text-black font-bold ">Visions:  </span>{projects.at(0).vision}</div>
+          {projects[0].tags
+            ?.split(',')
+            .filter((t) => t.length > 0)
+            .map((t) => {
+              return (
+                <div className="text-sm border-2 border-green text-green mb-4 px-2 rounded-md ">
+                  {' '}
+                  {t}{' '}
+                </div>
+              )
+            })}
         </div>
-        
-      </div>
-      <div className="rounded-xl mt-6 mb-16 overflow-hidden">
-        {projects.at(0).video.includes('youtu.be') || projects.at(0).video.includes('youtube') ? (
-          <ReactPlayer url={projects.at(0).video} muted={true} />
-        ) : (
-          <video className="w-full" controls>
-            <source src={projects.at(0).video} type="video/mp4" muted></source>
-          </video>
-        )}
-      </div>
-      </div>
-      <div className = "ml-8 App flex flex-col justify-start items-start" >
-      <span className=" text-xl mb-4 text-black font-bold "> Likes </span>
-      </div>
-      <div className='space-y-6 ml-8 App flex flex-col justify-start items-start'>
-        <span className=" text-xl mb-4 text-black font-bold "> comments </span>
-        <div className='flex flex-row justify-start items-start'>
-          <UserAvatar sizing className={'h-16 w-16'} link={connectedUser.picture} />
-          <div className='space-y-1 ml-2'>
-            <span className=" text-xl text-black font-bold ">{connectedUser.firstName} {connectedUser.lastName} </span>
-            <div className={'text-green-500 no-underline my-2'}>i love the idea!!</div>
-          </div>
-          
+        <div className="flex flex-row items-center">
+          <Location className="mr-1" />{' '}
+          <span className="my-2 text-dark">{projects[0].location}</span>
         </div>
-        <div className='flex flex-row justify-start items-start'>
-            <UserAvatar sizing className={'h-16 w-16'} link={connectedUser.picture} />
-            <div className='space-y-1 ml-2'>
-              <span className=" text-xl text-black font-bold ">{connectedUser.firstName} {connectedUser.lastName} </span>
-              <div className={'text-green-500 no-underline my-2'}>i love the idea!!</div>
+        <span className="mt-10 text-xl font-bold text-dark ">Description </span>
+        <div className={' my-2 text-dark'}>{projects[0].description}</div>
+        <div className="flex flex-col justify-start items-start mt-2">
+          <div className=" text-xl text-dark my-2">
+            <div className="my-2">
+              <span className=" text-xl font-bold">Field </span>
+              <span className="text-purple">{projects[0].field}</span>
+            </div>
+            <div className="my-2">
+              <span className=" text-xl font-bold ">Number of Employees </span>
+              <span className="text-purple">{projects[0].numberOfEmployees}</span>
+            </div>
+            <div className="my-2">
+              <span className=" text-xl font-bold ">Inception Date </span>
+              <span className="text-purple">
+                {projects[0].establishedOn
+                  ? projects[0].establishedOn.getDate() +
+                    '-' +
+                    projects[0].establishedOn.getMonth() +
+                    '-' +
+                    projects[0].establishedOn.getFullYear()
+                  : 'Not yet'}
+              </span>
+            </div>
+            <div className="my-2">
+              <span className=" text-xl font-bold">Capital </span>
+              <span className="text-purple"> {projects[0].estimatedCapital}$ </span>
             </div>
           </div>
-        
+        </div>
+        <div className="rounded-xl mt-6 mb-4 overflow-hidden">
+          {projects[0].video.includes('youtu.be') ||
+          projects[0].video.includes('youtube') ? (
+            <ReactPlayer url={projects[0].video} muted={true} />
+          ) : (
+            <video className="w-full" controls>
+              <source src={projects[0].video} type="video/mp4" muted></source>
+            </video>
+          )}
+        </div>
+        <div className="py-6 w-full">
+          <div className={'flex flex-row mt-6 text-sm font-light'}>
+            <Heart isClicked={reactions[0]} className="mx-1" />
+            <div
+              className="mr-2"
+              style={{
+                color: reactions[0] ? reactionsColors.like : reactionsColors.disabled,
+              }}
+            >
+              Like
+            </div>
+            <Comment isCommented={reactions[1]} className="mx-1" />
+            <div
+              className="mr-2"
+              style={{
+                color: reactions[1] ? reactionsColors.comment : reactionsColors.disabled,
+              }}
+            >
+              Comment
+            </div>
+            <Share isClicked={reactions[2]} className="mx-1" />
+            <div
+              className="mr-2"
+              style={{
+                color: reactions[2] ? reactionsColors.share : reactionsColors.disabled,
+              }}
+            >
+              Share
+            </div>
+            <Saved isClicked={reactions[3]} className="mx-1" />
+            <div
+              className="mr-2"
+              style={{
+                color: reactions[3] ? reactionsColors.save : reactionsColors.disabled,
+              }}
+            >
+              Save
+            </div>
+          </div>
+          <div className="mt-5 rounded-md  ">
+            {users
+              .slice(Math.floor(Math.random() * users.length - 1), users.length)
+              .map((u) => (
+                <PublicationComment
+                  user={u}
+                  content={
+                    sampleComments[Math.floor(Math.random() * sampleComments.length)]
+                  }
+                />
+              ))}
+          </div>
+        </div>
       </div>
-      
-      
     </>
   )
 }
--- src/components/Project.js
+++ src/components/Project.js
@@ -16,6 +16,8 @@ import { Button } from './Button'
 import { connectedUser } from '../data/user'
 import { reactionsColors } from '../data/general'
 import { deleteProject } from '../store/Projects/projects.actions'
+import Link from 'next/link'
+import ButtonArrow from '../assets/icons/ButtonArrow'
 
 const Project = ({ user, project, isOwnProject, deleteProject }) => {
   const [isDotsListOpen, setIsDotsListOpen] = useState(false)
@@ -122,43 +124,58 @@ const Project = ({ user, project, isOwnProject, deleteProject }) => {
           />
         )}
       </div>
-      <div className={'flex flex-row mt-7 text-sm font-light'}>
-        <Heart isClicked={reactions[0]} className="mx-1" />
-        <div
-          className="mr-2"
-          style={{
-            color: reactions[0] ? reactionsColors.like : reactionsColors.disabled,
-          }}
-        >
-          Like
+      <div
+        className={
+          'w-full flex flex-row justify-between items-center mt-7 text-sm font-light'
+        }
+      >
+        <div className="flex">
+          <Heart isClicked={reactions[0]} className="mx-1" />
+          <div
+            className="mr-2"
+            style={{
+              color: reactions[0] ? reactionsColors.like : reactionsColors.disabled,
+            }}
+          >
+            Like
+          </div>
+          <Comment isCommented={reactions[1]} className="mx-1" />
+          <div
+            className=" mr-2"
+            style={{
+              color: reactions[1] ? reactionsColors.comment : reactionsColors.disabled,
+            }}
+          >
+            Comment
+          </div>
+          <Share isClicked={reactions[2]} className="mx-1" />
+          <div
+            className=" mr-2"
+            style={{
+              color: reactions[2] ? reactionsColors.share : reactionsColors.disabled,
+            }}
+          >
+            Share
+          </div>
+          <Saved isClicked={reactions[3]} className="mx-1" />
+          <div
+            className=" mr-2"
+            style={{
+              color: reactions[3] ? reactionsColors.save : reactionsColors.disabled,
+            }}
+          >
+            Save
+          </div>
         </div>
-        <Comment isCommented={reactions[1]} className="mx-1" />
-        <div
-          className=" mr-2"
-          style={{
-            color: reactions[1] ? reactionsColors.comment : reactionsColors.disabled,
-          }}
-        >
-          Comment
-        </div>
-        <Share isClicked={reactions[2]} className="mx-1" />
-        <div
-          className=" mr-2"
-          style={{
-            color: reactions[2] ? reactionsColors.share : reactionsColors.disabled,
-          }}
-        >
-          Share
-        </div>
-        <Saved isClicked={reactions[3]} className="mx-1" />
-        <div
-          className=" mr-2"
-          style={{
-            color: reactions[3] ? reactionsColors.save : reactionsColors.disabled,
-          }}
-        >
-          Save
-        </div>
+        <Link href={'/project/' + project.id}>
+          <div className="text-purple flex items-center cursor-pointer">
+            <div>Learn more</div>
+            <ButtonArrow
+              className={'-rotate-90'}
+              color={tailwindConfig.theme.extend.colors.purple}
+            />
+          </div>
+        </Link>
       </div>
     </div>
   )
