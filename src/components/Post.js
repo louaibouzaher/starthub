c4278bb7447e029aea5b011b5e81c5ed9860b9de
MarioSass <mjs25@mail.aub.edu>
2022-03-06 21:34:32 +0200

    Post Details

--- src/components/Post.js
+++ src/components/Post.js
@@ -146,43 +146,58 @@ const Post = ({
       >
         {post.picture ? post.title : post.content}
       </div>
-      <div className={'flex flex-row mt-6 text-sm font-light'}>
-        <Heart isClicked={reactions[0]} className="mx-1" />
-        <div
-          className="mr-2"
-          style={{
-            color: reactions[0] ? reactionsColors.like : reactionsColors.disabled,
-          }}
-        >
-          Like
+      <div
+        className={
+          'w-full flex flex-row justify-between items-center mt-7 text-sm font-light'
+        }
+      >
+        <div className="flex">
+          <Heart isClicked={reactions[0]} className="mx-1" />
+          <div
+            className="mr-2"
+            style={{
+              color: reactions[0] ? reactionsColors.like : reactionsColors.disabled,
+            }}
+          >
+            Like
+          </div>
+          <Comment isCommented={reactions[1]} className="mx-1" />
+          <div
+            className=" mr-2"
+            style={{
+              color: reactions[1] ? reactionsColors.comment : reactionsColors.disabled,
+            }}
+          >
+            Comment
+          </div>
+          <Share isClicked={reactions[2]} className="mx-1" />
+          <div
+            className=" mr-2"
+            style={{
+              color: reactions[2] ? reactionsColors.share : reactionsColors.disabled,
+            }}
+          >
+            Share
+          </div>
+          <Saved isClicked={reactions[3]} className="mx-1" />
+          <div
+            className=" mr-2"
+            style={{
+              color: reactions[3] ? reactionsColors.save : reactionsColors.disabled,
+            }}
+          >
+            Save
+          </div>
         </div>
-        <Comment isCommented={reactions[1]} className="mx-1" />
-        <div
-          className="mr-2"
-          style={{
-            color: reactions[1] ? reactionsColors.comment : reactionsColors.disabled,
-          }}
-        >
-          Comment
-        </div>
-        <Share isClicked={reactions[2]} className="mx-1" />
-        <div
-          className="mr-2"
-          style={{
-            color: reactions[2] ? reactionsColors.share : reactionsColors.disabled,
-          }}
-        >
-          Share
-        </div>
-        <Saved isClicked={reactions[3]} className="mx-1" />
-        <div
-          className="mr-2"
-          style={{
-            color: reactions[3] ? reactionsColors.save : reactionsColors.disabled,
-          }}
-        >
-          Save
-        </div>
+        <Link href={'/post/' + post.id}>
+          <div className="text-purple flex items-center cursor-pointer">
+            <div>Learn more</div>
+            <ButtonArrow
+              className={'-rotate-90'}
+              color={tailwindConfig.theme.extend.colors.purple}
+            />
+          </div>
+        </Link>
       </div>
     </div>
   )
