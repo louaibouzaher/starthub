4a8f2e5551c197f7e57c8294807ba7c9187abc56
louaibouzaher <jm_bouzaher@esi.dz>
2022-03-10 23:57:39 +0200

    Edits to the profile page

--- pages/profile/[userId].js
+++ pages/profile/[userId].js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { useRouter } from 'next/router'
 import Head from 'next/head'
 import { connect } from 'react-redux'
@@ -12,64 +12,69 @@ import { Button } from '../../src/components/Button'
 import axios from 'axios'
 import { API_BASEURL } from '../../appConfig'
 
-function Profile({ sectionIndexer = { id: 0 }, user = {} }) {
+function Profile({ sectionIndexer, user = {}, connectedUser }) {
+  const isOwner = connectedUser.id == user.user.id
   const router = useRouter()
-  const { userId } = router.query
 
   return (
     <>
       <Head>
         <title>
-          {user.firstName} {user.lastName} - StartHub
+          {user.user.first_name} {user.user.last_name} - StartHub
         </title>
       </Head>
       <Navbar />
       <div className=" w-full pt-20 px-60 min-h-screen text-dark">
         <div className="flex justify-center items-start  rounded-md shadow-md py-6 px-4 ">
-          <UserAvatar sizing link={user.picture} className="h-32 w-32 my-2" />
-          <div className="flex flex-col justify-start items-start mx-10 p-2 w-2/5">
+          <UserAvatar sizing link={user.profilePic} className="h-32 w-32 my-2" />
+          <div className="flex flex-col justify-start items-start mx-10 p-2 w-1/2">
             <div className="text-2xl p-2 font-bold">
-              {user.firstName} {user.lastName}
+              {user.user.first_name} {user.user.last_name}
             </div>
             <div className="text-xs text-gray-400 px-2">{user.position}</div>
+            <div className="flex space-x-2 w-full mt-2">
+              <div>
+                <span className="text-purple">298</span> Followers
+              </div>
+              <div>
+                <span className="text-purple">843</span> Following
+              </div>
+              <div>
+                <span className="text-purple">10</span> Posts
+              </div>
+              <div>
+                <span className="text-purple">2</span> Projects
+              </div>
+            </div>
             <div className="text-sm p-2 break-words">{user.biography}</div>
             <div className="flex w-full mt-2">
-              <Button
-                btnStyle="w-1/2 bg-purple border-2 border-purple text-white hover:text-purple hover:bg-white mx-1"
-                label="Follow"
-                onClick={() => {
-                  console.log('Follow')
-                }}
-              />
-              <Button
-                btnStyle="w-1/2 bg-white border-2 border-purple text-purple mx-1"
-                label="Message"
-                onClick={() => {
-                  console.log('Follow')
-                }}
-              />
-            </div>
-          </div>
-          <div className="flex justify-center items-center mx-10 text-xl">
-            <div className="flex flex-col  justify-center item-center ">
-              <div className="flex flex-col items-center p-2">
-                <div>Followers</div>
-                <div className="text-purple">32</div>
-              </div>
-              <div className="flex flex-col items-center p-2">
-                <div>Following</div>
-                <div className="text-purple">763</div>
-              </div>
-            </div>
-            <div className="flex flex-col justify-center item-center p-2">
-              <div className="flex flex-col items-center p-2">
-                <div>Posts</div>
-                <div className="text-purple">4</div>
-              </div>
-              <div className="flex flex-col items-center p-2">
-                <div>Projects</div>
-                <div className="text-purple">1</div>
-              </div>
+              {!isOwner && (
+                <>
+                  <Button
+                    btnStyle="w-1/2 bg-purple border-2 border-purple text-white hover:text-purple hover:bg-white mx-1"
+                    label="Follow"
+                    onClick={() => {
+                      console.log('Follow')
+                    }}
+                  />
+                  <Button
+                    btnStyle="w-1/2 bg-white border-2 border-purple text-purple mx-1"
+                    label="Message"
+                    onClick={() => {
+                      console.log('Follow')
+                    }}
+                  />
+                </>
+              )}
+              {isOwner && (
+                <Button
+                  btnStyle="w-1/2 bg-white border-2 border-purple text-purple mx-1"
+                  label="Edit Profile"
+                  onClick={() => {
+                    router.push('/settings')
+                  }}
+                />
+              )}
             </div>
           </div>
         </div>
@@ -113,4 +118,15 @@ export async function getStaticProps({ params }) {
   }
 }
 
-export default Profile
+const mapStateToProps = (state) => {
+  return {
+    sectionIndexer: state.sectionIndexer,
+    connectedUser: state.user.data.connectedUser,
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Profile)
