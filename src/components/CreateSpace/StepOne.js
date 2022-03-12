1dd84baa51cd26c42c45036d3db2ba14a55babb7
louaibouzaher <jm_bouzaher@esi.dz>
2022-03-12 11:34:47 +0200

    Fixed structure in CreateSpace page

--- src/components/CreateSpace/StepOne.js
+++ src/components/CreateSpace/StepOne.js
@@ -3,7 +3,7 @@ import { Button } from '../Button'
 import Link from 'next/dist/client/link'
 import { useRouter } from 'next/router'
 
-export default function StepOne({setStep}) {
+export default function StepOne({ setStep }) {
   const router = useRouter()
 
   const [state, setState] = useState({
@@ -42,62 +42,36 @@ export default function StepOne({setStep}) {
   }
   return (
     <>
-      <div className="flex flex-col ">
-        <form>
-          <div class="flex flex-col justify-center items-center bg-white px-10 py-8 border-2 border-dark rounded-xl w-full shadow-md max-w mt-10">
-            <div>
-              <h1 class="text-center mb-6 text-2xl font-bold  text-dark">
-                Create Your Own Space
-              </h1>
-              <h2 className="font-bold">Space Information:</h2>
+      <div>
+        <label for="fname" class="block mb-1 text-dark ">
+          Space Title
+        </label>
+        <input
+          value={state.spaceTitle}
+          name="spaceTitle"
+          type="text"
+          class="bg-indigo-50 px-4 py-2 border-2 border-dark outline-none rounded-md w-full"
+          onChange={handleChange}
+        />
+        <div className="text-red-600 text-sm h-4">
+          {errors.spaceTitle && 'Title is required'}
+        </div>
+      </div>
 
-              <div>
-                <label for="fname" class="block mb-1 text-dark ">
-                  Space Title
-                </label>
-                <input
-                  value={state.spaceTitle}
-                  name="spaceTitle"
-                  type="text"
-                  class="bg-indigo-50 px-4 py-2 border-2 border-dark outline-none rounded-md w-full"
-                  onChange={handleChange}
-                />
-                <div className="text-red-600 text-sm h-4">
-                  {errors.spaceTitle && 'Title is required'}
-                </div>
-              </div>
-
-              <div className="mt-2">
-                <label for="TextArea" class="block mb-1 text-dark ">
-                  Space-Description
-                </label>
-                <textarea
-                  className="h-44 border-2 border-dark p-4 rounded-md bg-indigo-50  w-full"
-                  placeholder="Provide clear and consise description of your space."
-                  name="spaceDescription"
-                  onChange={handleChange}
-                />
-                <div className="text-red-600 text-sm h-4">
-                  {errors.spaceDescription && 'Discription is required'}
-                </div>
-              </div>
-            </div>
-
-            <div className=" flex flex-row py-1">
-              <Link href="/">
-                <Button
-                  label="Cancel"
-                  btnStyle={'border-2 border-dark mx-2 w-fit mr-20'}
-                />
-              </Link>
-              <Button
-                label="Next"
-                btnStyle="bg-purple text-white border-2 border-purple mx-2"
-                onClick={()=>setStep(1)}
-              />
-            </div>
-          </div>
-        </form>
+      <label for="TextArea" class="block mb-1 text-dark ">
+        Space Description
+      </label>
+      <textarea
+        style={{
+          resize: 'none',
+        }}
+        className="h-44 border-2 border-dark p-4 rounded-md bg-indigo-50  w-full"
+        placeholder="Provide clear and consise description of your space."
+        name="spaceDescription"
+        onChange={handleChange}
+      />
+      <div className="text-red-600 text-sm h-4">
+        {errors.spaceDescription && 'Discription is required'}
       </div>
     </>
   )
--- src/components/CreateSpace/StepTwo.js
+++ src/components/CreateSpace/StepTwo.js
@@ -2,102 +2,64 @@ import React, { useState } from 'react'
 import { Button } from '../Button'
 import Link from 'next/dist/client/link'
 import DesktopDatePicker from '@mui/lab/DesktopDatePicker'
-import Box from '@mui/material/Box';
+import Box from '@mui/material/Box'
 import Autocomplete from '@mui/material/Autocomplete'
 import { countries } from '../../data/countries'
 import AdapterDateFns from '@mui/lab/AdapterDateFns'
 import LocalizationProvider from '@mui/lab/LocalizationProvider'
 import TextField from '@mui/material/TextField'
 
-export default function StepTwo({
-  setSpaceE,
-  setSpace,
-  Space2,
-  Space,
-  setStep,
-}) {
+export default function StepTwo({ setSpaceE, setSpace, Space2, Space, setStep }) {
   const [isDatePickerDisabled] = useState(false)
 
   return (
     <>
-      <div className="flex flex-col ">
-        <form>
-          <div class="flex flex-col justify-center items-center bg-white px-10 py-8 border-2 border-dark rounded-xl w-full shadow-md max-w mt-10">
-            <div>
-              <h1 class="text-center mb-6 text-2xl font-bold  text-dark">
-                Create Your Own Space
-              </h1>
-              <h2 className="font-bold text-xl">Space Duration:</h2>
-
-              <div className="w-full my-2 p-4  shadow-md rounded-md ">
-                <div className="my-2 font-bold text-dark">Start Date</div>
-                <LocalizationProvider dateAdapter={AdapterDateFns}>
-                  <DesktopDatePicker
-                    disabled={isDatePickerDisabled}
-                    label="Select Date"
-                    inputFormat="MM/dd/yyyy"
-                    value={Space.establishedOn}
-                    onChange={(v) => {
-                      setSpace({ ...Space, establishedOn: v })
-                    }}
-                    renderInput={(params) => <TextField {...params} />}
-                  />
-                </LocalizationProvider>
-                <div className="my-2 font-bold text-dark">End Date</div>
-                <LocalizationProvider dateAdapter={AdapterDateFns}>
-                  <DesktopDatePicker
-                    disabled={isDatePickerDisabled}
-                    label="Select Date"
-                    inputFormat="MM/dd/yyyy"
-                    value={Space2.establishedOn}
-                    onChange={(newvalue) => {
-                      setSpaceE({ ...Space2, establishedOn: newvalue })
-                    }}
-                    renderInput={(param) => <TextField {...param} />}
-                  />
-                </LocalizationProvider>
-                <div className="my-2 font-bold text-dark">Location</div>
-                <Autocomplete
-                  disablePortal
-                  id="combo-box-demo"
-                  options={countries}
-                  sx={{ width: '95%' }}
-                  renderOption={(props, option) => (
-                    <Box component="li" sx={{ '& > img': { mr: 2, flexShrink: 0 } }} {...props}>
-                      <img
-                        loading="lazy"
-                        width="20"
-                        src={`https://flagcdn.com/w20/${option.code.toLowerCase()}.png`}
-                        srcSet={`https://flagcdn.com/w40/${option.code.toLowerCase()}.png 2x`}
-                        alt=""
-                      />
-                      {option.label} ({option.code})
-                    </Box>
-                  )}
-                  renderInput={(params) => (
-                    <TextField {...params} label="Select a country" />
-                  )}
-                />
-              </div>
-            </div>
-            <div className=" flex flex-row w-fit">
-            <Link href="/">
-                <Button label="Cancel" btnStyle={'border-2 border-dark mx-2 w-fit'} />
-              </Link>
-              <Button
-                label="Previous"
-                btnStyle="bg-white text-purple border-2 border-purple"
-                onClick={() => setStep(0)}
-              />
-              <Button
-                label="Next"
-                btnStyle="bg-purple text-white border-2 border-purple mx-2"
-                onClick={() => setStep(2)}
-              />
-            </div>
-          </div>
-        </form>
-      </div>
+      <div className="my-2 font-bold text-dark">Start Date</div>
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <DesktopDatePicker
+          disabled={isDatePickerDisabled}
+          label="Select Date"
+          inputFormat="MM/dd/yyyy"
+          value={Space.establishedOn}
+          onChange={(v) => {
+            setSpace({ ...Space, establishedOn: v })
+          }}
+          renderInput={(params) => <TextField {...params} />}
+        />
+      </LocalizationProvider>
+      <div className="my-2 font-bold text-dark">End Date</div>
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <DesktopDatePicker
+          disabled={isDatePickerDisabled}
+          label="Select Date"
+          inputFormat="MM/dd/yyyy"
+          value={Space2.establishedOn}
+          onChange={(newvalue) => {
+            setSpaceE({ ...Space2, establishedOn: newvalue })
+          }}
+          renderInput={(param) => <TextField {...param} />}
+        />
+      </LocalizationProvider>
+      <div className="my-2 font-bold text-dark">Location</div>
+      <Autocomplete
+        disablePortal
+        id="combo-box-demo"
+        options={countries}
+        sx={{ width: '40%' }}
+        renderOption={(props, option) => (
+          <Box component="li" sx={{ '& > img': { mr: 2, flexShrink: 0 } }} {...props}>
+            <img
+              loading="lazy"
+              width="20"
+              src={`https://flagcdn.com/w20/${option.code.toLowerCase()}.png`}
+              srcSet={`https://flagcdn.com/w40/${option.code.toLowerCase()}.png 2x`}
+              alt=""
+            />
+            {option.label} ({option.code})
+          </Box>
+        )}
+        renderInput={(params) => <TextField {...params} label="Select a country" />}
+      />
     </>
   )
 }
