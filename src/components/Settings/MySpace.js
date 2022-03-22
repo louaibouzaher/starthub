5e7369886cd57525ec30b223f1440f0076a8d98c
louaibouzaher <jm_bouzaher@esi.dz>
2022-03-22 21:17:00 +0200

    Linting

--- src/components/Settings/MySpace.js
+++ src/components/Settings/MySpace.js
@@ -3,7 +3,7 @@ import { space } from '../../data/space'
 
 export function MySpaces() {
   const sp = [space, space]
-  const pt=["Judge","Owner","Participant"]
+  const pt = ['Judge', 'Owner', 'Participant']
   return (
     <div className="flex w-fit flex-col justify-center ">
       {sp.map((space) => {
@@ -29,8 +29,10 @@ export function MySpaces() {
               </div>
             </div>
             <div className=" ml-5 w-1/5 text-2xl font-bold felx flex-col">
-              <h2 className='text-4xl'>Position</h2>
-              <div className='text-purple text-2xl py-8'>{pt[Math.floor(Math.random() * pt.length)].toString()}</div>
+              <h2 className="text-4xl">Position</h2>
+              <div className="text-purple text-2xl py-8">
+                {pt[Math.floor(Math.random() * pt.length)].toString()}
+              </div>
             </div>
           </div>
         )
