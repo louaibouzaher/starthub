34a923a2e9a8c6c970357c72b2dde55103d242fd
louaibouzaher <jm_bouzaher@esi.dz>
2022-03-22 17:16:15 +0200

    Added Home Page Title

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 import Navbar from '../src/components/Navbar'
 import { Hero } from '../src/components/Hero'
 import { SpaceMain } from '../src/components/SpaceMain'
+import Head from 'next/head'
 
 function Home() {
   return (
     <>
+      <Head>
+        <title>StartHub</title>
+      </Head>
       <Navbar />
       <Hero />
       <SpaceMain />
