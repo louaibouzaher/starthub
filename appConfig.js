2ff4c7a804d02b9b978309f9a92d7b34d4fa4693
MarioSass <mjs25@mail.aub.edu>
2022-03-30 00:51:04 +0300

    Message Component

--- appConfig.js
+++ appConfig.js
@@ -1,4 +1,5 @@
-export const API_BASEURL =
+export const API_BASEURL ='https://starthubapi.herokuapp.com/'
+/* 
   process.env.NODE_ENV == 'production'
-    ? 'https://starthubapi.herokuapp.com/'
-    : 'http://127.0.0.1:8000/'
+    ? 
+    : 'http://127.0.0.1:8000/'*/
