88bfa9506853429ab5c842c5758ef7e149d9067f
louaibouzaher <jm_bouzaher@esi.dz>
2022-04-04 23:51:22 +0300

    Edited Text in Landing Page

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,12 +4,12 @@ import { Button } from './Button'
 
 export const Hero = () => {
   return (
-    <div className="h-screen font-bold flex justify-center items-center p-20 overflow-x-hidden">
+    <div className="h-screen flex justify-center items-center p-20 overflow-x-hidden">
       <div className="flex flex-col w-1/2 justify-center items-start">
-        <div className="text-left font-sans text-7xl text-dark ">
+        <div className="text-left text-6xl text-dark font-bold">
           The Place For <br /> <span className="text-green">Growing</span> Startups
         </div>
-        <div className="w-1/2 text-left mt-5 text-dark">
+        <div className="w-1/2 text-left mt-5 text-dark font-thin">
           Join a community of entrepreneurs and businessmen shaping the market world.
         </div>
 
