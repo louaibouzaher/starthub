483f74485486e8716d122f67987b2e6d4a0f6c85
louaibouzaher <jm_bouzaher@esi.dz>
2022-04-04 23:27:19 +0300

    Minor edits to Listing Components

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,43 +1,35 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 import Post from './Post'
 import store from '../store'
 import { getPosts } from '../store/Posts/posts.api'
 
+function PostList({ posts }) {
+  useEffect(() => {
+    store.dispatch(getPosts())
+  }, [])
 
-
-function PostList ({posts})  {
-
-    useEffect(() => {
-        store.dispatch(getPosts())
-      }, [])
-
-    return (
-        posts?.map((p) => (
-            <Post
-              post={p}
-              user={{
-                id: p.owner?.id,
-                firstName: p.owner?.first_name,
-                lastName: p.owner?.last_name,
-                avatar: p.profile?.profilePic,
-              }}
-            />
-          ))
-    )
+  return posts?.map((p) => (
+    <Post
+      post={p}
+      user={{
+        id: p.owner?.id,
+        firstName: p.owner?.first_name,
+        lastName: p.owner?.last_name,
+        avatar: p.profile?.profilePic,
+      }}
+    />
+  ))
 }
 
 const mapStateToProps = (state) => {
-    return {
-      posts: state.posts.list
-    }
+  return {
+    posts: state.posts.list,
+  }
 }
 
 const mapDispatchToProps = (dispatch) => {
-    return {
-      changeChild: (newChild) => dispatch(changeChild(newChild)),
-      sectionsInit: (sections) => dispatch(sectionsInit(sections)),
-    }
-  }
-  
+  return {}
+}
+
 export default connect(mapStateToProps, mapDispatchToProps)(PostList)
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -4,41 +4,33 @@ import Project from './Project'
 import store from '../store'
 import { getProjects } from '../store/Projects/projects.api'
 
+function ProjectList({ projects }) {
+  useEffect(() => {
+    store.dispatch(getProjects())
+  }, [])
 
-
-function ProjectList ({projects})  {
-
-    useEffect(() => {
-        store.dispatch(getProjects())
-      }, [])
-
-    return (
-      projects?.map((p) => (
-        <Project
-          project={p}
-          user={{
-            id: p.owner?.id,
-            firstName: p.owner?.first_name,
-            lastName: p.owner?.last_name,
-            avatar: p.profile?.profilePic,
-            position: p.profile?.position,
-          }}
-        />
-      ))
-    )
+  return projects?.map((p) => (
+    <Project
+      project={p}
+      user={{
+        id: p.owner?.id,
+        firstName: p.owner?.first_name,
+        lastName: p.owner?.last_name,
+        avatar: p.profile?.profilePic,
+        position: p.profile?.position,
+      }}
+    />
+  ))
 }
 
 const mapStateToProps = (state) => {
-    return {
-      projects: state.projects.list
-    }
+  return {
+    projects: state.projects.list,
+  }
 }
 
 const mapDispatchToProps = (dispatch) => {
-    return {
-      changeChild: (newChild) => dispatch(changeChild(newChild)),
-      sectionsInit: (sections) => dispatch(sectionsInit(sections)),
-    }
-  }
-  
+  return {}
+}
+
 export default connect(mapStateToProps, mapDispatchToProps)(ProjectList)
