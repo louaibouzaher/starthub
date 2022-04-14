6fd2dc2cde8929d1e1819520b7298f4bb6bed320
Bilal <bhw03@mail.aub.edu>
2022-04-14 03:51:17 +0300

    Successful deploy of EmptyState

--- appConfig.js
+++ appConfig.js
@@ -1,4 +1,4 @@
-export const API_BASEURL ='https://starthubapi.herokuapp.com/'
-  // process.env.NODE_ENV == 'production'
-  //   ? 
-  //   : 'http://127.0.0.1:8000/'
+export const API_BASEURL =
+  process.env.NODE_ENV == 'production'
+    ? 'https://starthubapi.herokuapp.com/'
+    : 'http://127.0.0.1:8000/'
