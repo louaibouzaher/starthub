b90bf9fb0caa7268ddad619090d64f3a1215454e
louaibouzaher <jm_bouzaher@esi.dz>
2022-04-16 00:51:28 +0300

    Removed Decimals from money formatter

--- src/helpers/money.js
+++ src/helpers/money.js
@@ -1,7 +1,7 @@
-import React from 'react'
-
 export const money = (value) =>
   new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
   }).format(value)
